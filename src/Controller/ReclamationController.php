c4182a209aff374f1accad9ebe3fed89a9fea551
nafaa123 <nafaa.bensalem@esprit.tn>
2024-02-15 16:18:40 +0100

    migration etape 1

---
+++ src/Controller/ReclamationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reclamation;
+use App\Form\ReclamationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReclamationController extends AbstractController
+{
+    /**
+     * @Route("/reclamations", name="reclamation_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $reclamations = $this->getDoctrine()->getRepository(Reclamation::class)->findAll();
+
+        return $this->render('reclamation/index.html.twig', [
+            'reclamations' => $reclamations,
+        ]);
+    }
+
+    /**
+     * @Route("/reclamations/new", name="reclamation_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $reclamation = new Reclamation();
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($reclamation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('reclamation_index');
+        }
+
+        return $this->render('reclamation/new.html.twig', [
+            'reclamation' => $reclamation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // Actions edit, update, delete
+}
---
+++ src/Entity/Reclamation.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReclamationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\UX\Turbo\Attribute\Broadcast;
+
+#[ORM\Entity(repositoryClass: ReclamationRepository::class)]
+#[Broadcast]
+class Reclamation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $contenue = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $id_utilisateur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $statut = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContenue(): ?string
+    {
+        return $this->contenue;
+    }
+
+    public function setContenue(string $contenue): static
+    {
+        $this->contenue = $contenue;
+
+        return $this;
+    }
+
+    public function getIdUtilisateur(): ?string
+    {
+        return $this->id_utilisateur;
+    }
+
+    public function setIdUtilisateur(string $id_utilisateur): static
+    {
+        $this->id_utilisateur = $id_utilisateur;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
